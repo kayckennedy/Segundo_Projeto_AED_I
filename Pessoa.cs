b2911fd73ea9a0f6b2d487f2e8f8e491a3069db0
Deive <deivealtoe@gmail.com>
2019-12-08 17:19:01 -0300

    Atualizando Pessoa

--- Pessoa.cs
+++ Pessoa.cs
@@ -9,15 +9,19 @@ class Pessoa {
     public string GetNomeCompleto() {
         return this.nome_completo;
     }
+
     public string GetCpf() {
         return this.cpf;
     }
+
     public Data GetDataNascimento() {
         return this.data_nascimento;
     }
+
     public string GetEmail() {
         return this.email;
     }
+
     public string GetSenhaLogin() {
         return this.senha;
     }
@@ -25,15 +29,19 @@ class Pessoa {
     public void SetNomeCompleto(string nome_completo) {
         this.nome_completo = nome_completo;
     }
+
     public void SetCpf(string cpf) {
         this.cpf = cpf;
     }
+
     public void SetDataNascimento(Data data_nascimento) {
         this.data_nascimento = data_nascimento;
     }
+
     public void SetEmail(string email) {
         this.email = email;
     }
+    
     public void SetSenha(string senha) {
         this.senha = senha;
     }
